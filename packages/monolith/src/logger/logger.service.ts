b1b1a10e63642dadb9bb0e7d09ba88172a315e05
Phạm Thành Trung <adamward459@gmail.com>
2023-06-27 22:35:36 +0700

    Add logger service

---
+++ packages/monolith/src/logger/logger.service.ts
@@ -0,0 +1,74 @@
+import { EmbedBuilder } from '@discordjs/builders';
+import {
+  Client,
+  GatewayDispatchEvents,
+  GatewayIntentBits,
+} from '@discordjs/core';
+import { REST } from '@discordjs/rest';
+import { WebSocketManager } from '@discordjs/ws';
+import {
+  ConsoleLogger,
+  Injectable,
+  OnApplicationBootstrap,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class LoggerService
+  extends ConsoleLogger
+  implements OnApplicationBootstrap
+{
+  private client: Client;
+
+  constructor(private readonly configService: ConfigService) {
+    super();
+  }
+
+  async onApplicationBootstrap() {
+    const token = this.configService.get<string>('DISCORD_TOKEN') as string;
+    const rest = new REST({ version: '10' }).setToken(token);
+
+    const gateway = new WebSocketManager({
+      token,
+      intents:
+        GatewayIntentBits.GuildMessages | GatewayIntentBits.MessageContent,
+      rest,
+    });
+
+    this.client = new Client({ rest, gateway });
+    this.client.once(GatewayDispatchEvents.Ready, () => {
+      console.log('Ready');
+    });
+
+    await gateway.connect();
+
+    process.on('uncaughtException', (error) => {
+      this.error(error.message, error.stack, 'uncaughtException');
+    });
+  }
+
+  error(message: any, stack?: string, context?: string): void {
+    const embed = new EmbedBuilder();
+    embed.setColor(0xff0000);
+    embed.setTitle('Error');
+    embed.addFields(
+      {
+        name: `Message`,
+        value: message,
+      },
+      {
+        name: `Stack`,
+        value: stack ?? '',
+      },
+      {
+        name: `Context`,
+        value: context ?? '',
+      }
+    );
+
+    this.client.api.channels.createMessage(
+      this.configService.get<string>('DISCORD_CHANNEL_ID') as string,
+      { embeds: [embed.toJSON()] }
+    );
+  }
+}
