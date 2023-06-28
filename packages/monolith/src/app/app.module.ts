55f6778907ff5012c0333d1f225779a92c6f3be4
Phạm Thành Trung <adamward459@gmail.com>
2023-06-28 10:08:16 +0700

    Add logger service (#56)

--- packages/monolith/src/app/app.module.ts
+++ packages/monolith/src/app/app.module.ts
@@ -4,9 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    LoggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/monolith/src/logger/logger.service.ts
@@ -0,0 +1,96 @@
+import { EmbedBuilder } from '@discordjs/builders';
+import {
+  Client,
+  GatewayDispatchEvents,
+  GatewayIntentBits,
+} from '@discordjs/core';
+import { REST } from '@discordjs/rest';
+import { WebSocketManager } from '@discordjs/ws';
+import {
+  ConsoleLogger,
+  Injectable,
+  OnApplicationBootstrap,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class LoggerService
+  extends ConsoleLogger
+  implements OnApplicationBootstrap
+{
+  private client: Client;
+
+  constructor(private readonly configService: ConfigService) {
+    super();
+  }
+
+  async onApplicationBootstrap() {
+    const token = this.configService.get<string>('DISCORD_TOKEN') as string;
+    const rest = new REST({ version: '10' }).setToken(token);
+
+    const gateway = new WebSocketManager({
+      token,
+      intents:
+        GatewayIntentBits.GuildMessages | GatewayIntentBits.MessageContent,
+      rest,
+    });
+
+    this.client = new Client({ rest, gateway });
+    this.client.once(GatewayDispatchEvents.Ready, () => {
+      this.log('Discord bot is ready');
+    });
+
+    await gateway.connect();
+
+    process.on('uncaughtException', (error) => {
+      this.error(error.message, error.stack, 'uncaughtException');
+    });
+  }
+
+  log(message: any): void {
+    super.log(message);
+
+    const embed = new EmbedBuilder();
+    embed.setColor(0x1d7acb);
+    embed.setTitle('Info log');
+    embed.setDescription(message);
+    embed.setTimestamp();
+
+    this.sendMessageToDiscord(embed);
+  }
+
+  error(message: any, stack?: string, context?: string): void {
+    super.error(message, stack, context);
+
+    const embed = new EmbedBuilder();
+    embed.setColor(0xff0000);
+    embed.setTitle('Error log');
+    embed
+      .addFields(
+        {
+          name: `Message`,
+          value: message,
+        },
+        {
+          name: `Stack`,
+          value: stack ?? '',
+        },
+        {
+          name: `Context`,
+          value: context ?? '',
+        }
+      )
+      .setTimestamp();
+
+    this.sendMessageToDiscord(embed);
+  }
+
+  sendMessageToDiscord(embed: EmbedBuilder) {
+    if (this.configService.get('NODE_ENV') === 'development') {
+      return this.client.api.channels.createMessage(
+        this.configService.get<string>('DISCORD_DEV_CHANNEL_ID') as string,
+        { embeds: [embed.toJSON()] }
+      );
+    }
+  }
+}
